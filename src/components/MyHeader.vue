a6616fd88b4fc6dad97d4b285822892e6e4635b7
MLopez <codermex.web@gmail.com>
2024-09-19 16:55:40 -0300

    first commit

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <section class="">
+        <div class="m-5 flex">
+            <div class="py-10">
+                <h1 class=" text-left text-8xl font-black">DESARROLLADOR FULLSTACK</h1><!--underline  decoration-gray-400-->
+            </div>
+            <div>
+                <div class="bg-gradient-to-r from-violet-600 to-indigo-600 rounded-lg rounded-ss-3xl rounded-ee-3xl   ">
+                    <!-- <h1 class=" text-left font-black ">DESARROLLADOR FULLSTACK</h1> -->
+                    <img class="h-30 w-auto" src="../assets/fperfil.png"
+                    alt="Your Company" />
+                </div>
+            </div>
+        </div>
+        <div class=" bg-gray-900 mx-4 rounded-lg">
+            <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                <div class="relative flex h-16 items-center justify-between">
+                </div>
+            </div> 
+        </div>
+        <!-- 
+        </div> -->
+    </section>
+</template>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+    <MyNavbar/>
+    <MyHeader/>
+</template>
+<script setup lang="ts">
+import MyNavbar from '../components/MyNavbar.vue';
+import MyHeader from '../components/MyHeader.vue';
+
+</script>
