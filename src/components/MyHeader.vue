fd3cddcd21509e17538414c66a0b0f26df9f0210
MLopez <codermex.web@gmail.com>
2024-09-24 09:40:32 -0300

    #002commit - Cover / Header Design

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -2,7 +2,20 @@
     <section class="">
         <div class="m-5 flex">
             <div class="py-10">
-                <h1 class=" text-left text-8xl font-black">DESARROLLADOR FULLSTACK</h1><!--underline  decoration-gray-400-->
+                <h1 class="text-left text-8xl font-black uppercase mr-5">
+                    Hola, soy <span class="text-indigo-700">Marcos López</span>
+                </h1>
+                <div class="border-l-2 border-gray-300 ps-5 mt-4">
+                    <p class="text-left font-medium text-gray-500 text-xs uppercase">
+                        DESARROLLADOR FULLSTACK 
+                    </p>
+                    <p class="text-left font-medium text-gray-500 text-xs uppercase">
+                        Amante de la tecnología y la enseñanza
+                    </p>
+                    <p class="text-left font-medium text-gray-500 text-xs uppercase">
+                        Una buena UX es la que no te genera una pregunta.
+                    </p>
+                </div>
             </div>
             <div>
                 <div class="bg-gradient-to-r from-violet-600 to-indigo-600 rounded-lg rounded-ss-3xl rounded-ee-3xl   ">
