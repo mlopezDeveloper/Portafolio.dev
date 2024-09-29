a70201dae6888d7b26618c6cc810ce385b361212
MLopez <codermex.web@gmail.com>
2024-09-29 17:22:55 -0300

    #010commit - Get the api github - section project

--- src/components/MyProjects.vue
+++ src/components/MyProjects.vue
@@ -18,8 +18,21 @@
                         class="h-full w-full object-cover object-center"
                         @error="handleImageError(index)" /> <!-- Agregamos evento @error -->
                     <figcaption
-                        class="absolute px-4 text-lg text-white bg-gray-200 bottom-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                        <p>{{ repo.name }}</p>
+                        class="flex justify-around absolute w-full p-3 text-lg text-white bg-gradient-to-b from-transparent to-gray-700 bottom-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                        <p class="font-medium">{{ repo.description }}</p>
+                        <div class="flex items-center">
+                            <a v-if="repo.html_url" :href="repo.html_url" target="_blank" rel="noopener noreferrer" aria-label="Visitar sitio del proyecto" class="hover:text-indigo-400 transition-colors">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                                </svg>
+                            </a>
+                            <a class="pl-4 hover:text-indigo-400 transition-colors" v-if="repo.homepage" :href="repo.homepage" target="_blank" rel="noopener noreferrer" aria-label="Visitar sitio del proyecto">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+                                    <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+                                </svg>
+                            </a>
+                        </div>
                     </figcaption>
                 </figure>
             </div>
