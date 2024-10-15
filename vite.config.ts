142afb7d00b6ede0fbc77a689de30de703a18b25
MLopez <codermex.web@gmail.com>
2024-10-15 10:33:49 -0300

    #016commit -m Error produccion

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,21 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    compression({
+      // Opciones del plugin
+      verbose: true, // Muestra mensajes en la consola
+      disable: false, // Deshabilita si no quieres usar la compresión
+      threshold: 10240, // Archivos menores a 10KB no se comprimen
+      algorithm: 'gzip', // Algoritmo de compresión
+      ext: '.gz', // Extensión de los archivos comprimidos
+    }),
   ],
   resolve: {
     alias: {
